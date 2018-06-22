02638654ea8d94580d78afe3e905c4a66aae9fe7
92thunder <r.kunisada661@gmail.com>
2018-06-22 23:51:32 +0900

    Add prettier

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,38 +1,38 @@
 <template>
-    <div>
-        <div class="greeting">Hello {{name}}{{exclamationMarks}}</div>
-        <button @click="decrement">-</button>
-        <button @click="increment">+</button>
-    </div>
+  <div>
+    <div class="greeting">Hello {{name}}{{exclamationMarks}}</div>
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from "vue-property-decorator";
+import { Vue, Component, Prop } from 'vue-property-decorator'
 
 @Component
 export default class Hello extends Vue {
-    @Prop() name!: string;
-    @Prop() initialEnthusiasm!: number;
+  @Prop() name!: string
+  @Prop() initialEnthusiasm!: number
 
-    enthusiasm = this.initialEnthusiasm;
+  enthusiasm = this.initialEnthusiasm
 
-    increment() {
-        this.enthusiasm++;
+  increment() {
+    this.enthusiasm++
+  }
+  decrement() {
+    if (this.enthusiasm > 1) {
+      this.enthusiasm--
     }
-    decrement() {
-        if (this.enthusiasm > 1) {
-            this.enthusiasm--;
-        }
-    }
+  }
 
-    get exclamationMarks(): string {
-        return Array(this.enthusiasm + 1).join('!');
-    }
+  get exclamationMarks(): string {
+    return Array(this.enthusiasm + 1).join('!')
+  }
 }
 </script>
 
 <style>
 .greeting {
-    font-size: 20px;
+  font-size: 20px;
 }
 </style>
