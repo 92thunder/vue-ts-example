b16a7c6b0069f8ebe9ae0d9316919cbbf3137626
92thunder <r.kunisada661@gmail.com>
2018-06-22 23:23:41 +0900

    Add vue-property-decorator.

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -7,29 +7,28 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { Vue, Component, Prop } from "vue-property-decorator";
 
-export default Vue.extend({
-    props: ['name', 'initialEnthusiasm'],
-    data() {
-        return {
-            enthusiasm: this.initialEnthusiasm,
-        }
-    },
-    methods: {
-        increment() { this.enthusiasm++; },
-        decrement() {
-            if (this.enthusiasm > 1) {
-                this.enthusiasm--;
-            }
-        },
-    },
-    computed: {
-        exclamationMarks(): string {
-            return Array(this.enthusiasm + 1).join('!');
+@Component
+export default class Hello extends Vue {
+    @Prop() name!: string;
+    @Prop() initialEnthusiasm!: number;
+
+    enthusiasm = this.initialEnthusiasm;
+
+    increment() {
+        this.enthusiasm++;
+    }
+    decrement() {
+        if (this.enthusiasm > 1) {
+            this.enthusiasm--;
         }
     }
-});
+
+    get exclamationMarks(): string {
+        return Array(this.enthusiasm + 1).join('!');
+    }
+}
 </script>
 
 <style>
